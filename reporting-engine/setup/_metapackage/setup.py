c4fe4e206e27f09db96b245b01837cd9b57f8784
alessandro-fiorino <alessandro.fiorino@digitaldomus.it>
2021-04-27 12:19:12 +0200

    Importazione reporting-engine 2021-04-27

---
+++ reporting-engine/setup/_metapackage/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read().strip()
+
+setuptools.setup(
+    name="odoo14-addons-oca-reporting-engine",
+    description="Meta package for oca-reporting-engine Odoo addons",
+    version=version,
+    install_requires=[
+        'odoo14-addon-base_comment_template',
+        'odoo14-addon-bi_view_editor',
+        'odoo14-addon-kpi_dashboard',
+        'odoo14-addon-report_qweb_encrypt',
+        'odoo14-addon-report_qweb_pdf_watermark',
+        'odoo14-addon-report_xlsx',
+        'odoo14-addon-report_xlsx_helper',
+        'odoo14-addon-report_xlsx_helper_demo',
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Odoo',
+    ]
+)
