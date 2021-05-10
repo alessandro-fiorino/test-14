9a9bb88049ecc14d27e755ab8570948408b29910
alessandro-fiorino <alessandro.fiorino@digitaldomus.it>
2021-05-10 19:23:43 +0200

    Rimosso openupgradelib

--- l10n-italy/l10n_it_withholding_tax_reason/__init__.py
+++ l10n-italy/l10n_it_withholding_tax_reason/__init__.py
@@ -3,28 +3,28 @@
 
 from . import models
 
-from openupgradelib import openupgrade
+#from openupgradelib import openupgrade
 
 
-def rename_old_italian_module(cr):
+#def rename_old_italian_module(cr):
 
-    if not openupgrade.is_module_installed(cr, "l10n_it_withholding_tax_causali"):
-        return
+#    if not openupgrade.is_module_installed(cr, "l10n_it_withholding_tax_causali"):
+#        return
 
-    openupgrade.rename_xmlids(
-        cr,
-        [
-            (
-                "l10n_it_withholding_tax_causali.view_withholding_tax_form_causale",
-                "l10n_it_withholding_tax_causali.view_withholding_tax_form_reason",
-            ),
-        ],
-    )
+#    openupgrade.rename_xmlids(
+#        cr,
+#        [
+#            (
+#                "l10n_it_withholding_tax_causali.view_withholding_tax_form_causale",
+#                "l10n_it_withholding_tax_causali.view_withholding_tax_form_reason",
+#            ),
+#        ],
+#    )
 
-    openupgrade.update_module_names(
-        cr,
-        [
-            ("l10n_it_withholding_tax_causali", "l10n_it_withholding_tax_reason"),
-        ],
-        merge_modules=True,
-    )
+#    openupgrade.update_module_names(
+#        cr,
+#        [
+#            ("l10n_it_withholding_tax_causali", "l10n_it_withholding_tax_reason"),
+#        ],
+#        merge_modules=True,
+#    )
