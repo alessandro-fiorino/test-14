9229fbf88e5d0e6c491ee1f404960ec2a4dc49e0
alessandro-fiorino <alessandro.fiorino@digitaldomus.it>
2021-07-07 16:10:04 +0200

    Bump to 14

---
+++ web_image_sign/__manifest__.py
@@ -0,0 +1,21 @@
+# See LICENSE file for full copyright and licensing details.
+
+{
+    "name": "Web Image Signature",
+    "version": "14.0.1.0.0",
+    "author": "Serpent Consulting Services Pvt. Ltd., Digital Domus s.n.c.",
+    "depends": ["web"],
+    "license": "AGPL-3",
+    "category": "Tools",
+    "description": """
+     This module provides the functionality to store digital signature
+    """,
+    "summary": """
+        Touch screen enable so user can add signature with touch devices.
+        Digital signature can be very usefull for documents - Compatible with EE
+    """,
+    "data": ["views/assets.xml"],
+    "qweb": ["static/src/xml/digital_sign.xml"],
+    "installable": True,
+    "auto_install": False,
+}
